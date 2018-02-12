a1ae802c16401ec362a187f38f6fdc5f93c7409d
clg <clg_dream@163.com>
2018-02-12 11:38:36 +0800

    HenCoder 修改username

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
@@ -21,7 +21,7 @@ import static com.hencoder.hencoderpracticedraw6.Utils.dpToPixel;
 public class Practice01Translation extends RelativeLayout {
     Button animateBt;
     ImageView imageView;
-
+    int conten;
     public Practice01Translation(Context context) {
         super(context);
     }
@@ -49,6 +49,8 @@ public class Practice01Translation extends RelativeLayout {
             @Override
             public void onClick(final View v) {
                 // TODO 在这里处理点击事件，通过 View.animate().translationX/Y/Z() 来让 View 平移
+
+
             }
         });
     }
